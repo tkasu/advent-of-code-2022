e09efcecdfddd00e60a46944b1866f98ce9bb8cd
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2022-12-02 11:43:49 +0200

    Add day2.

---
+++ src/main/scala/tkasu/aoc22/challenges/day2/part1.scala
@@ -0,0 +1,98 @@
+package tkasu.aoc22.challenges.day2
+
+import cats.effect.{IO, IOApp, Resource}
+import tkasu.aoc22.utils.files.{closeFile, makeSourceResource, readLines, readResource}
+
+import java.io.File
+import scala.io.Source
+
+object part1 extends IOApp.Simple {
+
+  private val inputResource = makeSourceResource("day2/part1/input")
+
+  enum RPCInput:
+    case Rock, Paper, Scissors
+
+  object RPCInput:
+    def fromOppRaw(s: String): RPCInput =
+      s match {
+        case "A" => RPCInput.Rock
+        case "B" => RPCInput.Paper
+        case "C" => RPCInput.Scissors
+        case _ => throw IllegalArgumentException(s"Invalid opponent input: $s")
+      }
+
+    def fromMyRaw(s: String): RPCInput =
+      s match {
+        case "X" => RPCInput.Rock
+        case "Y" => RPCInput.Paper
+        case "Z" => RPCInput.Scissors
+        case _ => throw IllegalArgumentException(s"Invalid my input: $s")
+      }
+
+  case class RPCRound(otherInput: RPCInput, myInput: RPCInput) {
+
+    def shapeScore: Int = this.myInput match {
+      case RPCInput.Rock => 1
+      case RPCInput.Paper => 2
+      case RPCInput.Scissors => 3
+    }
+
+    def perfScore = this match {
+      case RPCRound(other, my) if other == my => 3
+      case RPCRound(RPCInput.Rock, my) => my match {
+        case RPCInput.Paper => 6
+        case RPCInput.Scissors => 0
+      }
+      case RPCRound(RPCInput.Paper, my) => my match {
+        case RPCInput.Scissors => 6
+        case RPCInput.Rock => 0
+      }
+      case RPCRound(RPCInput.Scissors, my) => my match {
+        case RPCInput.Rock => 6
+        case RPCInput.Paper => 0
+      }
+    }
+
+    def score: Int =
+      shapeScore + perfScore
+  }
+
+  object RPCRound:
+
+    /**
+     * Parse input from AoC input format:
+     * e.g. 'A X'
+     * First other players is my input:
+     * A -> Rock
+     * B -> Paper
+     * C -> Scissors
+     * After space, the second latter is my input:
+     * X -> Rock
+     * Y -> Paper
+     * Z -> Scissors
+     * */
+    def fromRaw(lineInput: String): RPCRound =
+      val (oppRaw, myRaw) = lineInput.split(" ") match {
+        case Array(fst, snd, _*) => (fst, snd)
+        case _ => throw IllegalArgumentException(s"Invalid input row: $lineInput")
+      }
+
+      val oppInput = RPCInput.fromOppRaw(oppRaw)
+      val myInput = RPCInput.fromMyRaw(myRaw)
+      RPCRound(oppInput, myInput)
+
+  def parseFile(): IO[Seq[RPCRound]] = for {
+    input <- inputResource.use(src => readLines(src))
+    rounds = input
+      .split("\n")
+      .map(RPCRound.fromRaw)
+      .toSeq
+  } yield rounds
+
+    override def run = {
+    parseFile()
+      .map(_.map(_.score).sum)
+      .map(println)
+  }
+}
---
+++ src/main/scala/tkasu/aoc22/challenges/day2/part2.scala
@@ -0,0 +1,76 @@
+package tkasu.aoc22.challenges.day2
+
+import cats.effect.{IO, IOApp, Resource}
+import tkasu.aoc22.challenges.day2.part1.{RPCInput, RPCRound}
+import tkasu.aoc22.utils.files.{closeFile, makeSourceResource, readLines, readResource}
+
+import java.io.File
+import scala.io.Source
+
+object part2 extends IOApp.Simple {
+
+  private val inputResource = makeSourceResource("day2/part1/input")
+
+  enum RoundResult:
+    case Lose, Draw, Win
+
+  object RoundResult:
+    def fromRaw(s: String): RoundResult =
+      s match {
+        case "X" => RoundResult.Lose
+        case "Y" => RoundResult.Draw
+        case "Z" => RoundResult.Win
+        case _ => throw IllegalArgumentException(s"Invalid result input: $s")
+      }
+
+  def fromResultRaw(s: String, oppInput: RPCInput): RPCInput =
+    RoundResult.fromRaw(s) match {
+      case RoundResult.Draw => oppInput
+      case RoundResult.Win => oppInput match {
+        case RPCInput.Rock => RPCInput.Paper
+        case RPCInput.Paper => RPCInput.Scissors
+        case RPCInput.Scissors => RPCInput.Rock
+      }
+      case RoundResult.Lose => oppInput match {
+        case RPCInput.Rock => RPCInput.Scissors
+        case RPCInput.Paper => RPCInput.Rock
+        case RPCInput.Scissors => RPCInput.Paper
+      }
+    }
+
+  /**
+   * Parse input from AoC input format:
+   * e.g. 'A X'
+   * First other players is my input:
+   * A -> Rock
+   * B -> Paper
+   * C -> Scissors
+   * After space, the desired result for the round:
+   * X -> Lose
+   * Y -> Draw
+   * Z -> Win
+   * */
+  def roundFromRaw(lineInput: String): RPCRound =
+    val (oppRaw, resultRaw) = lineInput.split(" ") match {
+      case Array(fst, snd, _*) => (fst, snd)
+      case _ => throw IllegalArgumentException(s"Invalid input row: $lineInput")
+    }
+
+    val oppInput = RPCInput.fromOppRaw(oppRaw)
+    val myInput = fromResultRaw(resultRaw, oppInput)
+    RPCRound(oppInput, myInput)
+
+  def parseFile(): IO[Seq[RPCRound]] = for {
+    input <- inputResource.use(src => readLines(src))
+    rounds = input
+      .split("\n")
+      .map(roundFromRaw)
+      .toSeq
+  } yield rounds
+
+  override def run = {
+    parseFile()
+      .map(_.map(_.score).sum)
+      .map(println)
+  }
+}
